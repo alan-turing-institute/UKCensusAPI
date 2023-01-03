202398eba6e0bd0d398d6c9a06563fbe353bdc10
AoifeHughes <ahughes@turing.ac.uk>
2023-01-03 14:14:24 +0000

    change to zip extracting

--- ukcensusapi/NRScotland.py
+++ ukcensusapi/NRScotland.py
@@ -141,8 +141,9 @@ class NRScotland:
     """
 
     if not os.path.exists(os.path.join(str(self.cache_dir), table + ".csv")):
-      _ = zipfile.ZipFile(str(self.__source_to_zip(NRScotland.data_sources[NRScotland.GeoCodeLookup[resolution]])))
-      with zipfile.ZipFile(os.path.join(str(self.cache_dir), table + ".csv"),"r") as zip_ref:
+      zf = zipfile.ZipFile(str(self.__source_to_zip(NRScotland.data_sources[NRScotland.GeoCodeLookup[resolution]])))
+      with zipfile.ZipFile(zf,"r") as zip_ref:
+        print("Extracting downloaded zip...")
         zip_ref.extractall()
     raw_data = pd.read_csv(os.path.join(str(self.cache_dir), table + ".csv"))
     # more sophisticate way to check for no data?
